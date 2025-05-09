c14b976d0770fdd71e77b725824ea8207f82cd29
ekolvah <ekolvah@gmail.com>
2025-05-09 17:37:10 +0300

    Обновлен файл конфигурации развертывания в Screeps: добавлена переменная окружения SCREEPSDEPLOY_CONFIG_PATH и изменен способ вызова команды развертывания для указания сервера.

---
+++ screeps-deploy.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  servers: {
+    main: {
+      host: 'screeps.com',
+      port: 443,
+      secure: true,
+      token: process.env.SCREEPS_TOKEN,
+      branch: 'main'
+    }
+  }
+}; 
